d3b428ec3237d98668446f6469cd1a59be8acc17
emilyhamberg <emily.hamberg@gmail.com>
2022-08-03 16:22:26 -0500

    update imports

--- WeddingPageTests/WebDriverFactory.cs
+++ WeddingPageTests/WebDriverFactory.cs
@@ -3,7 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
-using WeddingPageTests.Variables;
+using WeddingPageTests.Config;
 
 namespace WeddingPageTests
 {
